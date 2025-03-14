6f80bdcc5bbe9839f645f30b954d0acb864c56c4
Gerardo <technicalzfg@gmail.com>
2025-03-14 17:33:22 -0400

    fix: adjust font sizes in CardUI component for improved readability and consistency

--- src/components/ui/CardUI.vue
+++ src/components/ui/CardUI.vue
@@ -21,17 +21,17 @@ function handleSubmit(): void {
     @submit.prevent="handleSubmit"
     class="w-full pt-10 pb-20 px-4 bg-[#2B2F3B] rounded-2xl"
   >
-    <h2 class="font-bold text-xl xl:text-3xl mb-8">{{ title }}</h2>
+    <h2 class="text-xl xl:text-3xl mb-8">{{ title }}</h2>
 
     <input
       :type="type"
       :placeholder="placeholder"
-      class="w-full px-4 py-6 bg-[#252832] rounded-lg mb-6"
+      class="w-full px-4 py-6 bg-[#252832] rounded-lg mb-6 text-sm"
     />
 
     <button
       type="submit"
-      class="bg-linear-90 from-[#D64059] to-[#5E489D] text-white font-bold px-16 py-3.5 rounded-lg w-full flex justify-center cursor-pointer"
+      class="bg-linear-90 from-[#D64059] to-[#5E489D] text-white font-bold px-16 py-2 rounded-lg w-full flex justify-center cursor-pointer text-sm"
     >
       {{ buttonText }}
     </button>
